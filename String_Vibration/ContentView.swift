03af8da4e5de6bddcaa5abe1b81e58cc210c6f33
Cong Le <longchike@gmail.com>
2025-06-28 00:39:33 -0700

    initial setup

---
+++ String_Vibration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  String_Vibration
+//
+//  Created by Cong Le on 6/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ String_Vibration/String_VibrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  String_VibrationApp.swift
+//  String_Vibration
+//
+//  Created by Cong Le on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct String_VibrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
